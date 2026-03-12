297234a246c2c23a19f3157a0552d4511e36fda1
Jonny Mortemore <jonathanmortemore@gmail.com>
2026-03-12 15:11:01 +1100

    update: ship can take a size
    Reject a size less than one
    Tested

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,16 @@
 export class Ship {
-    constructor() {
+    constructor(size) {
+        this.size = size;
+    }
 
+    get size() {
+        return this._size;
+    }
+
+    set size(size) {
+        if (size < 1 ) {
+            throw new RangeError('ship cannot be sized less than 1')
+        }
+        this._size = size;
     }
 }
--- tests/projectTesting.test.js
+++ tests/projectTesting.test.js
@@ -3,3 +3,10 @@ import {Ship} from "../src/ship.js"
 test('ship class exists', () => {
     expect(new Ship()).toBeTruthy
 })
+
+test('ship sized correctly', () => {
+    expect(new Ship(4).size).toBe(4),
+    expect(new Ship(8).size).toBe(8),
+    expect(() => new Ship(0)).toThrow(RangeError)
+    expect(() => new Ship(-1)).toThrow(RangeError)
+})
