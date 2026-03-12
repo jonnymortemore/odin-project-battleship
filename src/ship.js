3af786e50c2548074e3a977197cb2fa3b0bb520a
Jonny Mortemore <jonathanmortemore@gmail.com>
2026-03-12 15:34:15 +1100

    update: basic ship functionality
    length and hits and the ability to be sunk

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,8 @@
 export class Ship {
     constructor(size) {
         this.size = size;
+        this.hits = 0
+        this.sunk = false
     }
 
     get size() {
@@ -12,5 +14,24 @@ export class Ship {
             throw new RangeError('ship cannot be sized less than 1')
         }
         this._size = size;
+    }
+
+    get hits() {
+        return this._hits
+    }
+
+    set hits(value) {
+        this._hits = value
+    }
+
+    hit() {
+        this.hits += 1
+        if (this.hits === this.size) {
+            this.sinkShip()
+        }
+    }
+
+    sinkShip() {
+        this.sunk = true;
     }
 }
--- tests/projectTesting.test.js
+++ tests/projectTesting.test.js
@@ -1,12 +1,32 @@
-import {Ship} from "../src/ship.js"
+import { Ship } from "../src/ship.js";
 
-test('ship class exists', () => {
-    expect(new Ship()).toBeTruthy
-})
+test("ship class exists", () => {
+    expect(new Ship()).toBeTruthy;
+});
 
-test('ship sized correctly', () => {
-    expect(new Ship(4).size).toBe(4),
-    expect(new Ship(8).size).toBe(8),
-    expect(() => new Ship(0)).toThrow(RangeError)
-    expect(() => new Ship(-1)).toThrow(RangeError)
+test("ship sized correctly", () => {
+    expect(new Ship(4).size).toBe(4);
+    expect(new Ship(8).size).toBe(8);
+    expect(() => new Ship(0)).toThrow(RangeError);
+    expect(() => new Ship(-1)).toThrow(RangeError);
+});
+
+test("ships should take hits", () => {
+    const ship = new Ship(4);
+    expect(ship.hits).toBe(0);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+});
+
+test("ships can sink", () => {
+    const ship = new Ship(3);
+    expect(ship.sunk).toBe(false)
+    //direct ship sinking
+    ship.sinkShip();
+    expect(ship.sunk).toBe(true);
+    //ship sinking using hit
+    const newShip = new Ship(2);
+    newShip.hit()
+    newShip.hit()
+    expect(ship.sunk).toBe(true)
 })
